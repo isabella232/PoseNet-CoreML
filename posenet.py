49a8674eddc96550bd47e2354809ede04a658b56
Joaquin Alori <joaquin.alori@gmail.com>
2018-08-20 17:28:00 -0300

    Copy hyperparameters from original implementation and other improvements

--- posenet.py
+++ posenet.py
@@ -23,11 +23,18 @@ INPUT_IMAGE_PATH = "converter/images/tennis_in_crowd.jpg"
 OUTPUT_IMAGE_PATH = "output.jpg"
 
 K_LOCAL_MAXIMUM_RADIUS = 1  # TODO: WHAT? They use 1 in implementation but not in paper
-PERSON_SCORE_THRESHOLD = 0.4
-PART_SCORE_THRESHOLD = 0.5
-NMS_RADIUS = 20  # TODO: Not used?? the same as K_LOCAL_MAXIMUM_RADIUS?
+PERSON_SCORE_THRESHOLD = 0.15
+PART_SCORE_THRESHOLD = 0.1
+NMS_RADIUS = 30  # TODO: Not used?? the same as K_LOCAL_MAXIMUM_RADIUS?
 OUTPUT_STRIDE = 16
 MAX_POSE_DETECTIONS = float('inf')
+INPUT_MULTIPLIER = 1  # Multiply with respect to original implementation's input resolution
+INPUT_WIDTH = int(INPUT_MULTIPLIER * 289)
+INPUT_HEIGHT = int(INPUT_MULTIPLIER * 241)
+OUTPUT_VIDEO_SCALE = 3
+PARTS_TO_DETECT = [
+    'nose', 'leftEye', 'rightEye', 'leftEar', 'rightEar', 'leftShoulder', 'rightShoulder'
+]
 
 
 def toOutputStridedLayers(convolutionDefinition, outputStride):
@@ -197,7 +204,6 @@ def score_is_maximum_in_local_window(keypoint_id, score, heatmap_y, heatmap_x,
 with open(CONFIG_PATH, "r+") as f:
     cfg = yaml.load(f)
 checkpoints = cfg['checkpoints']
-imageSize = cfg['imageSize']
 chk = cfg['chk']
 outputStride = OUTPUT_STRIDE
 chkpoint = checkpoints[chk]
@@ -208,9 +214,6 @@ elif chkpoint == 'mobilenet_v1_075':
     mobileNetArchitectures = cfg['mobileNet75Architecture']
 else:
     mobileNetArchitectures = cfg['mobileNet100Architecture']
-
-width = imageSize
-height = imageSize
 
 
 # Load weights from harddrive into `variables` list
@@ -227,7 +230,7 @@ for x in variables:
     d = tf.reshape(d, variables[x]["shape"])
     variables[x]["x"] = tf.Variable(d, name=x)
 
-image = tf.placeholder(tf.float32, shape=[1, imageSize, imageSize, 3], name='image')
+image = tf.placeholder(tf.float32, shape=[1, INPUT_HEIGHT, INPUT_WIDTH, 3], name='image')
 x = image
 
 # Define base network and load its weights
@@ -262,29 +265,34 @@ with tf.Session() as sess:
     frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     fps = cap.get(cv2.CAP_PROP_FPS)
-    delta_t = 1 / fps
-    frame_counter = 0
+    timers = {
+        'total': 0.0, 'frame_read': 0.0, 'resize_input': 0.0, 'model': 0.0, 'decode': 0.0,
+        'draw_heatmap': 0.0, 'draw_keypoints': 0.0, 'resize_output': 0.0, 'write_frame': 0.0
+    }
     video_progress_bar = click.progressbar(length=total_frames)
 
     # Setup Output Video
     output_video_path = sys.argv[2] if len(sys.argv) > 2 else None
     if output_video_path:
         fourcc = cv2.VideoWriter_fourcc(*'XVID')
-        out = cv2.VideoWriter(sys.argv[2], fourcc, fps, (width, height))  # Width, Height
+        out = cv2.VideoWriter(
+            sys.argv[2], fourcc, fps,
+            (INPUT_WIDTH * OUTPUT_VIDEO_SCALE, INPUT_HEIGHT * OUTPUT_VIDEO_SCALE)
+        )
 
     # Generate video
     with video_progress_bar as progress_bar:
         start = time.time()
-        for _ in progress_bar:
-            frame_counter += 1
+        for i, _ in enumerate(progress_bar):
             # Read frame
-            start_frame = time.time()
+            frame_start = time.time()
             ret, frame = cap.read()
             if frame is None:
                 break
 
             # Resize input
-            frame = cv2.resize(frame, (width, height))
+            resize_input_start = time.time()
+            frame = cv2.resize(frame, (INPUT_WIDTH, INPUT_HEIGHT))
             input_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             input_image = input_image.astype(float)
             input_image = input_image * (2.0 / 255.0) - 1.0
@@ -296,28 +304,28 @@ with tf.Session() as sess:
             heatmaps_result, offsets_result, displacements_fwd_result, displacements_bwd_result = sess.run(  # noqa
                 [heatmaps, offsets, displacements_fwd, displacements_bwd], feed_dict={image: input_image})  # noqa
             after_detect = time.time()
-            model_end = time.time()
 
             # Generate poses from model's output
+            decode_start = time.time()
             poses = decode_multiple_poses(
                 heatmaps_result[0], offsets_result[0], displacements_fwd_result[0],
                 displacements_bwd_result[0], OUTPUT_STRIDE, MAX_POSE_DETECTIONS,
                 PERSON_SCORE_THRESHOLD, PART_SCORE_THRESHOLD, NMS_RADIUS
             )
-            decode_end = time.time()
-            # print(f"{model_end - model_start:.4f} - {decode_end - model_end:.3f}")
 
-            if False:
+            draw_heatmap_start = time.time()
+            if True:
                 # Draw heatmap
-                heatmap_index = 11  # Change this value to change what to draw
+                heatmap_index = 5  # Change this value to change what to draw
                 heatmaps_img = (heatmaps_result * 255).astype(np.uint8)[:, :, :, heatmap_index][0]
                 resized_heatmaps_img = cv2.resize(
-                    heatmaps_img, tuple(image.shape[1:3].as_list()), interpolation=cv2.INTER_CUBIC
+                    heatmaps_img, (INPUT_WIDTH, INPUT_HEIGHT), interpolation=cv2.INTER_CUBIC
                 )
                 frame = (
                     0.7 * np.expand_dims(resized_heatmaps_img, axis=2) + 0.3 * frame
                 ).astype(np.uint8)
 
+            draw_keypoints_start = time.time()
             # Draw keypoints
             child_to_parent_map = {
                 child: part_name_to_id_map[parent] for parent, child in pose_chain
@@ -325,8 +333,7 @@ with tf.Session() as sess:
             input_image = frame
             for person in poses:
                 color = (255, 255, 255)
-                # color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-                line_width = 2
+                line_width = 1
                 for keypoint in person['keypoints']:
                     if keypoint['score'] < PART_SCORE_THRESHOLD:
                         continue
@@ -341,6 +348,8 @@ with tf.Session() as sess:
                         cv2.putText(input_image, f"{person['score']:.2f}", text_position,
                                     cv2.FONT_HERSHEY_SIMPLEX, 0.4, color, 1, cv2.LINE_AA)
                     else:
+                        if keypoint['part'] not in PARTS_TO_DETECT:
+                            continue
                         parent_point_idx = child_to_parent_map[keypoint['part']]
                         parent_point = person['keypoints'][parent_point_idx]
                         cv2.line(
@@ -351,17 +360,40 @@ with tf.Session() as sess:
                             line_width
                         )
             frame = frame.squeeze()
-            if False:
+            resize_output_start = time.time()
+            frame = cv2.resize(frame, None, fx=OUTPUT_VIDEO_SCALE, fy=OUTPUT_VIDEO_SCALE)
+            stream_video = False
+            write_frame_start = time.time()
+            if stream_video:
                 cv2.imshow('image', frame)
                 cv2.waitKey(1)
-            if True:
+            else:
                 out.write(frame)
-                end = time.time()
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
+            frame_end = time.time()
 
+            if not i == 0:  # Skip first frame as network is slow to start up.
+                timers['total'] += frame_end - frame_start
+                timers['frame_read'] += resize_input_start - frame_start
+                timers['resize_input'] += model_start - resize_input_start
+                timers['model'] += decode_start - model_start
+                timers['decode'] += draw_heatmap_start - decode_start
+                timers['draw_heatmap'] += draw_keypoints_start - draw_heatmap_start
+                timers['draw_keypoints'] += resize_output_start - draw_keypoints_start
+                timers['resize_output'] += write_frame_start - resize_output_start
+                timers['write_frame'] += frame_end - write_frame_start
+
+    end = time.time()
     if output_video_path:
         out.release()
     cap.release()
     cv2.destroyAllWindows()
-    print("fps {:2f}".format(frame_counter / (end - start)))
+
+    # Profiling
+    sorted_timers = sorted(timers.items(), key=lambda t: t[1], reverse=True)
+    for key, value in sorted_timers:
+            print(f"avg {key:<15} {value / total_frames:>10.4f}s"
+                  f"{(value / timers['total']) * 100:>10.2f}%"
+                  f"{1 / (value / total_frames):>10.0f}fps")
+
